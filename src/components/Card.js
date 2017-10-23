36ed455d057b438cacec622d528d11a79cc201c6
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-10-23 14:39:14 -0700

    removed subfolder, main folder is the react project

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Card = ({ link, title, subtitle }) => (
+  <a href={ link }>
+    <div className='card card-styles'>
+        <div className='card-body'>
+          <h4 className='card-title'>{ title }</h4>
+          <p className='card-subtitle muted-text'>{ subtitle }</p>
+        </div>
+    </div>
+  </a>
+);
+
+Card.propTypes = {
+  link: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired
+}
+
+export default Card;
